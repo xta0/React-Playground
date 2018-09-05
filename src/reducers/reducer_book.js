e698b2424a9fcf1cba212aabccf74149e92b60c4
xta0 <jayson.xu@foxmail.com>
2018-09-05 17:11:20 -0400

    Update(auto commit)

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function BookReducer() {
+  return [
+    { title: 'JS: The Good Parts' },
+    { title: 'Hary Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ];
+}
